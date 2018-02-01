2b2020a33fdb90ccce91125315adcec38cba7577
gb23 <gregbenj@hotmail.com>
2018-02-01 11:25:37 -0700

    implement redux store, provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+    
+);
 registerServiceWorker();
+
+// making an application-wide state:
+// var store = {
+//     sections: [{},{} ],
+//     sectionFormData: [{text: "", votes: 0, next_ids: [-1]}],
+//     currentSection: {}
+// }
