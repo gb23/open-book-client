2794511955aa2c4cb1a2dba9819f7aaf6810a436
gb23 <gregbenj@hotmail.com>
2018-02-02 10:06:56 -0700

    add upvoting feature

--- src/components/SectionCard.js
+++ src/components/SectionCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const SectionCard = (props) => (
-    <div key={props.section.id} className="SectionCard">
-        <h3>{props.section.text}</h3>
-        <p>Votes: {props.section.votes}</p>
-    </div> 
-)
+const SectionCard = (props) => {
+
+    // const handleUpVote = () => {
+    //     props.onVote(props.section.id);
+    // }
+
+    return (
+        <div key={props.section.id} className="center mw5 mw6-ns br3 hidden ba b--black-10 SectionCard">
+            <h1 className="SectionTop f6 br3  br--top bg-near-black white mv0 pv2 ph3">{props.section.id}</h1>
+            <div className="pa3 bt">
+                <p className="f6 f5-ns lh-copy measure mv0">{props.section.text}</p>
+                <a onClick={ () => props.onVote(props.section.id)} className="link dim" href="#0"><i className="fas fa-thumbs-up"  /></a>
+            </div>
+            {props.section.votes}
+        </div> 
+    );
+}
 
 export default SectionCard;
