abde9f2923bc265e7f517a0663c72d206be681e5
gb23 <gregbenj@hotmail.com>
2018-02-05 16:26:23 -0700

    integrate sectionCurrent into redux state

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { createStore,applyMiddleware,combineReducers } from 'redux'
 import thunk from 'redux-thunk';
 import sections from './reducers/sections';
 import sectionFormData from './reducers/sectionFormData';
+import sectionCurrent from './reducers/sectionCurrent';
 
 const middleware = [thunk];
 
 const reducers = combineReducers({
     sections,
-    sectionFormData
+    sectionFormData,
+    sectionCurrent
 })
 
 export default createStore(
