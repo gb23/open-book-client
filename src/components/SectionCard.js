e52f8909a1f9def1ff1361be030f1d16720db8c7
gb23 <gregbenj@hotmail.com>
2018-02-05 16:28:46 -0700

    pass in props to handle navigation functions

--- src/components/SectionCard.js
+++ src/components/SectionCard.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const SectionCard = (props) => {
-
-    // const handleUpVote = () => {
-    //     props.onVote(props.section.id);
-    // }
-
     return (
-        <div key={props.section.id} className="center mw5 mw6-ns br3 hidden ba b--black-10 SectionCard">
+        <div autoFocus={props.autoFocus} key={props.section.id} className="center mw5 mw6-ns br3 hidden ba b--black-10 SectionCard" tabIndex="0" onKeyDown={(event) => props.onDown(event, props.section)}
+        
+        
+        onClick={ (event) => props.onSelect(event, props.section) }> 
             <h1 className="SectionTop f6 br3  br--top bg-near-black white mv0 pv2 ph3">{props.section.id}</h1>
             <div className="pa3 bt">
                 <p className="f6 f5-ns lh-copy measure mv0">{props.section.text}</p>
