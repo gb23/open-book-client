811bf3ee53cdd299fe80bea87f2ceb1e9a9cfad0
gb23 <gregbenj@hotmail.com>
2018-02-07 14:54:10 -0700

    comment out unnecessary scenarios

--- src/reducers/sectionCurrent.js
+++ src/reducers/sectionCurrent.js
@@ -3,10 +3,10 @@ const initialState = {id: -1};
 
 export default ( state = initialState , action) => {
     switch(action.type){
-        case 'SECTION_RIGHT':
-            return {...action.section}
-        case 'SECTION_LEFT':
-            return {...action.section}
+       // case 'SECTION_RIGHT':
+         //   return {...action.section}
+        //case 'SECTION_LEFT':
+         //   return {...action.section}
         case 'SECTION_SET':
             return {...action.section}
         default:
