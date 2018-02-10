72f25f60b3f46a0052bc37fe457fae9f482899bc
gb23 <gregbenj@hotmail.com>
2018-02-09 19:54:40 -0700

    allow form, upon submission, to set this new form as the current section

--- src/containers/SectionForm.js
+++ src/containers/SectionForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { updateSectionFormData } from '../actions/sectionForm'
-import { createSection } from '../actions/sections'
+import { createSection, setCurrentSection, replaceFormWithSection } from '../actions/sections'
 
 // doesn't have to be a class.  is a stateless
 class SectionForm extends Component {
@@ -14,16 +14,32 @@ class SectionForm extends Component {
     }
     
     handleOnSubmit = (event) => {
+
         event.preventDefault();
         const props = this.props;  
+        
         const sectionFormData = {...this.props.sectionFormData, prev_id: props.sectionToAddTo }
-       
+       //debugger
+       //make section current this soon-to-be-created newly id
+       //since the object hasn't been created yet, we will create a mock object based on the id value it will receive
+       //so we need to find the greatest id value in this.props.sections and add 1 to it.
+      
+        this.props.setCurrentSection({id: this.getGreatestSectionsId() + 1})
+       //make redux store sectionReplace.valid = false
+        this.props.replaceFormWithSection({valid: false})
         this.props.createSection(sectionFormData);
     }
+
+    getGreatestSectionsId = () => parseInt(this.props.sections.slice(-1)[0].id, 10)
+
     render(){
         const { text } = this.props.sectionFormData
+       // debugger;
         return(
-            <div className="center mw5 mw6-ns br3 hidden ba b--black-10 SectionCard">
+            <div tabIndex="0" className="center mw5 mw6-ns br3 hidden ba b--black-10 SectionCard" 
+            onKeyDown={this.props.onDown ? (event) => this.props.onDown(event, this.props.section) : () => ""}
+            onClick={this.props.onSelect ? (event) => this.props.onSelect(event, this.props.section) : () => ""}
+            >
              <h1 className="SectionTop f6 br3  br--top bg-near-black white mv0 pv2 ph3">Add a new section</h1>
                 <form onSubmit={this.handleOnSubmit}>
                     {/* <input type="hidden" name="prev_id" value={}/> */}
@@ -46,4 +62,4 @@ const mapStateToProps = state => {
         sectionFormData: state.sectionFormData
     }
 }
-export default connect(mapStateToProps, {updateSectionFormData, createSection})(SectionForm);
+export default connect(mapStateToProps, { setCurrentSection, updateSectionFormData, replaceFormWithSection, createSection})(SectionForm);
