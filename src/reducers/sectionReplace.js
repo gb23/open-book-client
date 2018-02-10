f6c82e296fe206ca6bc5e1401bdc5f30f0b0c0d7
gb23 <gregbenj@hotmail.com>
2018-02-09 19:32:57 -0700

    add SECTION_RERENDER for when form is switched back to a section card

--- src/reducers/sectionReplace.js
+++ src/reducers/sectionReplace.js
@@ -6,6 +6,8 @@ export default ( state = initialState , action) => {
     switch(action.type){
         case 'SECTION_REPLACE':
             return action.replacementInfo
+        case 'SECTION_RERENDER':
+            return action.replacementInfo
         default:
             return state;
     }
