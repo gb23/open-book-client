fcc9146e5fe51fc7b856d7ad5b7496470fe1dbb2
gb23 <gregbenj@hotmail.com>
2018-02-15 05:44:37 -0700

    add row position numbers

--- src/components/SectionCard.js
+++ src/components/SectionCard.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 const SectionCard = (props) => {
     return (
-        <div  key={props.section.id} className="center mw5 mw6-ns br3 hidden ba b--black-10 SectionCard" 
+        <div  
+            key={props.section.id} className="center mw5 mw6-ns br3 hidden ba b--black-10 SectionCard" 
             tabIndex="0" onKeyDown={(event) => props.onDown(event, props.section)}
-             onClick={ (event) => props.onSelect(event, props.section) }
-             //onFocus={ (event) => props.onSelect(event, props.section) }
+            onClick={ (event) => props.onSelect(event, props.section) }
             ref={ props.divRef }
         > 
          
             <h1 className="SectionTop f6 br3  br--top bg-near-black white mv0 pv2 ph3">{props.section.id}</h1>
             <div className="pa3 bt">
                 <p className="f6 f5-ns lh-copy measure mv0">{props.section.text}</p>
-                <a onClick={ () => props.onVote(props.section.id)} className="link dim" href="#0"><i className="fas fa-thumbs-up"  /></a>
+             
+                <a onClick={ () => props.onVote(props.section.id)} className="link dim" href="#0"><i className="fas fa-thumbs-up"  />{props.section.votes}</a>
+                
+                <span style={{color: 'blue', marginLeft: '150px'}}>{props.location ? `${props.location}/${props.total}` : ""}</span>
+              
             </div>
-            {props.section.votes}
+            
         </div> 
     );
 }
