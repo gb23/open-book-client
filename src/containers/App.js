736b91422f8d34f3be739413fa5541efdbe492ce
gb23 <gregbenj@hotmail.com>
2018-02-19 10:04:15 -0700

    Add mock routes for NavBar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,62 +1,26 @@
 import React, { Component } from 'react';
-//import './App.css';
-
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
+import NavBar from '../components/NavBar'
 import Sections from './Sections.js';
 
-//import {connect} from 'react-redux';
-
-//const API_URL = process.env.REACT_APP_API_URL;
-
-// const sections = [
-//     {
-//         text: "one text",
-//         votes: 1,
-//         next_ids: [2],
-//     },
-//     {
-//         text: "two text",
-//         votes: 2,
-//         next_ids: [3]
-//     },
-//     {
-//         text: "three text",
-//         votes: 2,
-//         next_ids: [-1]
-//     },
-// ];
-
-
-// Really should be in components section because stateless and just rendering info
 class App extends Component {
-    // constructor(props){
-    //     super(props);
 
-    //     this.state = {
-    //         sections: [],
-    //         loading: true
-    //     }
-    // }
-//     componentDidMount(){
-//       
-//         // fetch(`${API_URL}/sections`)
-//         //     .then(response => response.json())
-//         //     //.then(sections => this.setState({sections: sections, loading: false }))
-//    }
     render() {
         return (
-            <div className="App">
-            {/* { this.state.loading ?
-                <h1>Loading...</h1>
-                :
-                <Sections sections={this.state.sections} />
-            } */}
-                <Sections />
-            </div>
+            <Router>
+                <div className="App">
+                    <NavBar />
+                    {/* <Route exact path="/" render={()=> <div>home</div>} /> */}
+                    <Route exact path="/" render={() => <Sections/>} />
+                    <Route exact path="/about" render={() => <div>About</div>} />
+                    <Route exact path="/stitch" render={() => <div>Stitch</div>} />
+                </div>
+            </Router>
         );
     }
 }
 
 export default App;
-// function mapStateToProps(state) {
-//     console.log('in map state to props')
-//     return {sections: state.sections}
