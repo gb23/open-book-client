542cc82567715322b22d48e9de6d6afce3953258
gb23 <gregbenj@hotmail.com>
2018-02-21 15:31:08 -0700

    set up routes with appropriate redirects

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Route
-} from 'react-router-dom'
+import {Route, Redirect, Switch} from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux'
 import NavBar from '../components/NavBar'
+import About from '../components/About'
+import NotFound from '../components/NotFound'
 import Sections from './Sections.js';
+import { history } from '../store.js'
 
 class App extends Component {
 
     render() {
+        //debugger;
         return (
-            <Router>
+            <ConnectedRouter history={history}>
                 <div className="App">
                     <NavBar />
-                    {/* <Route exact path="/" render={()=> <div>home</div>} /> */}
-                    <Route exact path="/" render={() => <Sections/>} />
-                    <Route exact path="/about" render={() => <div>About</div>} />
-                    <Route exact path="/stitch" render={() => <div>Stitch</div>} />
+                    <Switch>
+                        <Route exact path="/" render={() => <Redirect to="/about" />} />
+                        <Route exact path="/compositions" render={() => <Redirect to="/compositions/:id" />} />
+                        <Route exact path="/compositions/:id" component={Sections} />
+                        
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/stitch" render={() => <div>Stitch</div>} />
+                        <Route path="*" component={NotFound} />
+                    </Switch>
                 </div>
-            </Router>
+            </ConnectedRouter>
         );
     }
 }
