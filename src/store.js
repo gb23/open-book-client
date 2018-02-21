b19125c1f43672b58421edb212327a026671068b
gb23 <gregbenj@hotmail.com>
2018-02-21 15:29:41 -0700

    add routerMiddleware(history) to middleware

--- src/store.js
+++ src/store.js
@@ -4,19 +4,27 @@ import sections from './reducers/sections';
 import sectionFormData from './reducers/sectionFormData';
 import sectionCurrent from './reducers/sectionCurrent';
 import composition from './reducers/composition';
+import {  routerMiddleware} from 'react-router-redux'
+//, push 
+//routerReducer,
+import createHistory from 'history/createBrowserHistory'
 //import sectionPath from './reducers/sectionPath';
-
-const middleware = [thunk];
+export const history = createHistory()
+const middleware = [thunk, routerMiddleware(history)];
 
 const reducers = combineReducers({
     sections,
     sectionFormData,
     sectionCurrent,
-    composition
+    composition,
+   
 })
+// router: routerReducer
 
-export default createStore(
+const store = createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(...middleware)
 );
+//store.dispatch(push('/about'))
+export default store;
