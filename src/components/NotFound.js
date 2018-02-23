c7a3cd3691668b7f07d37f9f00cd5f3ac8fc0243
gb23 <gregbenj@hotmail.com>
2018-02-22 17:51:00 -0700

    create a not found 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div> Not Found! </div>
+        <div>
+            
+            <div className="center SectionCard bt bb b--black-10">
+                <h2 className="notFound biggerText yellow">4</h2>
+                
+            </div>
+            <div className="center SectionCard bt bb b--black-10">
+                <h2 className="notFound biggerText  dark-red">0</h2>
+        
+            </div>
+            <div className="center SectionCard bt bb b--black-10">
+                <h2 className="notFound biggerText  navy">4</h2>
+
+                
+            </div>
+            <p className="f5 notFoundTitle avenir">Opps.  We can't find this :( </p>
+        </div>
     );
 }
 
