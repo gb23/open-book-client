956435b88a29f5a850e5ed43b8a55afd6d4284a5
gb23 <gregbenj@hotmail.com>
2018-02-23 11:27:20 -0700

    handle invalid form submission

--- src/containers/SectionForm.js
+++ src/containers/SectionForm.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { updateSectionFormData } from '../actions/sectionForm'
 import { createSection, setCurrentSection } from '../actions/sections'
+import { resetSectionForm } from '../actions/sectionForm'
 import { setComposition } from '../actions/composition';
 import { push } from 'react-router-redux';
 
 class SectionForm extends Component {
+    // constructor(props){
+    //     super(props)
+    //     this.warning = false;
+    // }
+  
     handleOnChange = event => {
         const { name, value } = event.target;
         const currentSectionFormData = Object.assign({}, this.props.sectionFormData, {
@@ -16,22 +22,37 @@ class SectionForm extends Component {
     
     handleOnSubmit = (event) => {
         event.preventDefault();  
-        const sectionFormData = {...this.props.sectionFormData, prev_id: parseInt(this.props.sectionToAddTo, 10) }
-        this.props.createSection(sectionFormData)
-        //
-
-        const nextId = parseInt(this.getGreatestSectionsId() + 1, 10);
-        const nextSection = {id: nextId, ...sectionFormData}
-        this.props.setCurrentSection({...nextSection, valid: false});
-        if(nextSection.prev_id === -1){
-            this.props.setComposition({ids: [...this.props.composition.ids, nextSection.id] , currentId: nextSection.id});
-            this.props.push(`/compositions/${this.props.composition.ids.length + 1}`)
+        if (this.props.sectionFormData.text.trim().length !== 0){
+            const sectionFormData = {...this.props.sectionFormData, prev_id: parseInt(this.props.sectionToAddTo, 10) }
+            this.props.createSection(sectionFormData)
+ 
+            const nextId = parseInt(this.getGreatestSectionsId() + 1, 10);
+            const nextSection = {id: nextId, ...sectionFormData}
+            this.props.setCurrentSection({...nextSection, valid: false});
+            if(nextSection.prev_id === -1){
+                this.props.setComposition({ids: [...this.props.composition.ids, nextSection.id] , currentId: nextSection.id});
+                this.props.push(`/compositions/${this.props.composition.ids.length + 1}`)
+            }
         }
+        else {
+            
+            const textAreaClassOriginal = this.formElement.firstElementChild.className;
+            const textAreaClassNew = textAreaClassOriginal + " warningRed"
+            this.formElement.firstElementChild.className = textAreaClassNew;
+            setTimeout(() => {
+                this.formElement.firstElementChild.className = textAreaClassOriginal;
+            }, 100);
+
+            this.props.resetSectionForm();
+            
+            
+        }
+        
     }
 
     handleButtonColor = (name) => {
         
-        let classString = "f6 link dim br1 ph3 pv2 mb2 dib white ";
+        let classString = "fade f6 link br1 ph3 mb2 dib white ";
         if (name === "ADD CONTENT"){
             classString += "bg-navy"
         }
@@ -41,7 +62,7 @@ class SectionForm extends Component {
         }
         else {
             // ADD COMPOSITION
-            classString += "bg-yellow"
+            classString += "bg-gold"
         }
         return classString;
 
@@ -59,15 +80,15 @@ class SectionForm extends Component {
             onClick={this.props.onSelect ? (event) => this.props.onSelect(event, this.props.section) : () => ""}
             ref={ this.props.divRef }
             >
-             <h5 className="sans-serif">{this.props.name}</h5>
-                <form onSubmit={this.handleOnSubmit}>
-                    <label htmlFor="text">Text:</label>
+             {/* <h5 className="sans-serif">{this.props.name}</h5> */}
+                <form ref={el => this.formElement = el}className="formPaddingTop pl2 pr2 pb1" onSubmit={this.handleOnSubmit}>
                     
-                    <textarea className="" onChange={this.handleOnChange} 
+                    
+                    <textarea className="w-100 fixTextArea avenir bg-near-white" onChange={this.handleOnChange} 
                                 onClick={(event) => event.stopPropagation()} 
                                 type="text" name="text" value={text} 
                     />
-                    <div>
+                    <div className="cardData reverseFlexDirection">
                         <button className={this.handleButtonColor(this.props.name)} type="submit" >{`${this.props.name}`}</button>
                     </div>
                 </form>
@@ -84,4 +105,4 @@ const mapStateToProps = state => {
         composition: state.composition
     }
 }
-export default connect(mapStateToProps, { setCurrentSection, updateSectionFormData, createSection, setComposition, push })(SectionForm);
+export default connect(mapStateToProps, { setCurrentSection, updateSectionFormData, createSection, setComposition, push, resetSectionForm })(SectionForm);
