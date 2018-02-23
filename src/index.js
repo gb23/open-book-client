200a2a7841ee5fff16c07584914bf4159f4eb33c
gb23 <gregbenj@hotmail.com>
2018-02-22 17:49:20 -0700

    import font-awesome file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js';
-
+import 'font-awesome/css/font-awesome.min.css'
 
 ReactDOM.render(
     
