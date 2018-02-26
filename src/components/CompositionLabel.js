9253227ac31fda01a058515fbee6fcb258cec54c
gb23 <gregbenj@hotmail.com>
2018-02-26 15:30:57 -0700

    prevent label showing on about url with compose composition

--- src/components/CompositionLabel.js
+++ src/components/CompositionLabel.js
@@ -4,7 +4,7 @@ const CompositionLabel = (props) => {
     let current = props.comp.ids.indexOf(props.comp.currentId) + 1
     let length = props.comp.ids.length;
     let returnStatement = ""
-    if(props.urlId !== "new" && props.urlId !== ":id"){
+    if(props.urlId !== "new" && props.urlId !== ":id" && props.sectionId !== 0){
         returnStatement = 
         ( 
             <div className="compositionLabel">
