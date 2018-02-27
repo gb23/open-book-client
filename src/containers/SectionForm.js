1b29d873b220e8cc75ff10002614d67428c2d58e
gb23 <gregbenj@hotmail.com>
2018-02-27 11:35:03 -0700

    handle submission of sandboxed 'about' data

--- src/containers/SectionForm.js
+++ src/containers/SectionForm.js
@@ -23,15 +23,30 @@ class SectionForm extends Component {
     handleOnSubmit = (event) => {
         event.preventDefault();  
         if (this.props.sectionFormData.text.trim().length !== 0){
+            //debugger;
             const sectionFormData = {...this.props.sectionFormData, prev_id: parseInt(this.props.sectionToAddTo, 10) }
-            this.props.createSection(sectionFormData)
+            if(this.props.url && this.props.url.params.id === "about"){
+                this.props.createSection({...sectionFormData, about: true})
+            }
+            else {
+                this.props.createSection(sectionFormData)
+            }
+           
  
             const nextId = parseInt(this.getGreatestSectionsId() + 1, 10);
             const nextSection = {id: nextId, ...sectionFormData}
             this.props.setCurrentSection({...nextSection, valid: false});
-            if(nextSection.prev_id === -1){
+            if(nextSection.prev_id === -1 || nextSection.prev_id === -2 ){
                 this.props.setComposition({ids: [...this.props.composition.ids, nextSection.id] , currentId: nextSection.id});
-                this.props.push(`/compositions/${this.props.composition.ids.length + 1}`)
+                if(this.props.url && this.props.url.params.id === "about"){
+                    //debugger;
+                    //this.props.push(`/compositions/about`);  
+                }
+                else{
+                    this.props.push(`/compositions/${this.props.composition.ids.length + 1}`)
+                }
+
+                
             }
         }
         else {
@@ -69,15 +84,22 @@ class SectionForm extends Component {
 
     }
 
+    handlePlaceholder = (section) => {
+        return "hello!"
+    }
+
     getGreatestSectionsId = () => parseInt(this.props.sections.list.slice(-1)[0].id, 10)
 
     render(){
         const { text } = this.props.sectionFormData
-
+        let placeHolder = "";
+        if(this.props.name === "ADD COMPOSITION"){
+            placeHolder = "Give your new composition a title or other descriptive attribute."
+        }
         return(
-            <div tabIndex="0" className="center SectionCard bt bb b--black-10" 
-            onKeyDown={this.props.onDown ? (event) => this.props.onDown(event, this.props.section) : () => ""}
-            onClick={this.props.onSelect ? (event) => this.props.onSelect(event, this.props.section) : () => ""}
+            <div className="center SectionCard bt bb b--black-10" 
+           // onKeyDown={this.props.onDown ? (event) => this.props.onDown(event, this.props.section) : () => ""}
+           //onClick={this.props.onSelect ? (event) => this.props.onSelect(event, this.props.section) : () => ""}
             ref={ this.props.divRef }
             >
              {/* <h5 className="sans-serif">{this.props.name}</h5> */}
@@ -85,7 +107,31 @@ class SectionForm extends Component {
                     
                     
                     <textarea className="w-100 fixTextArea avenir bg-near-white" onChange={this.handleOnChange} 
-                                onClick={(event) => event.stopPropagation()} 
+                                placeholder={placeHolder}
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    this.props.onSelect(event, this.props.section)
+                                }}
+                                onKeyDown={(event) => {
+                                    //const props = this.props;
+                                    if (event.key === "ArrowRight" ||event.key === "ArrowLeft" || event.key === "ArrowUp" || event.key === "ArrowDown" ){
+                                        if (this.props.sectionFormData.text === ""){
+                                            this.props.onDown(event, this.props.section)
+                                        }
+                                        else {
+                                            let position = this.formElement.firstChild.selectionStart;
+                                            if (position === this.props.sectionFormData.text.length && (event.key === "ArrowRight" || event.key === "ArrowDown")){
+                                                console.log("trying to go right or down")
+                                                this.props.onDown(event, this.props.section)
+                                            }
+                                            else if (position === 0 && (event.key === "ArrowUp" || event.key === "ArrowLeft")){
+                                                console.log("trying to go left or up")
+                                                this.props.onDown(event, this.props.section)
+                                            }
+                                            
+                                        }
+                                    }
+                                }} 
                                 type="text" name="text" value={text} 
                     />
                     <div className="cardData reverseFlexDirection">
