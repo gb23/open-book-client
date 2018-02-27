f6e440db439919ea9dfd2632185f4f0d4b62f54a
gb23 <gregbenj@hotmail.com>
2018-02-27 11:32:59 -0700

    allow CompositionLabel to label creation of new composition

--- src/components/CompositionLabel.js
+++ src/components/CompositionLabel.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
 const CompositionLabel = (props) => {
+    let current = props.comp.ids.indexOf(props.comp.currentId) + 1
+    let length = props.comp.ids.length;
     let returnStatement = ""
-    if(props.urlId !== "new" && props.urlId !== ":id"){
-        returnStatement = 
-        ( 
-            <div className="compositionLabel">
-                Composition {props.urlId} of {props.totalNumberComp}
-            </div>
-        )
+    if(props.urlId !== ":id" && props.urlId !== "new" ){
+        if(props.section.id !== 0 || (props.section.id === 0 && props.section.prev_id !== -1 && props.section.prev_id !== -2)){
+            returnStatement = 
+            ( 
+                <div className="compositionLabel">
+                    Composition {current} of {length}
+                </div>
+            )
+        }
+        else {
+            returnStatement = 
+            ( 
+                <div className="compositionLabel">
+                    Create Composition {length + 1}
+                </div>
+            )
+        }  
     }
+    else if (props.urlId === "new"){
+            returnStatement = 
+            ( 
+                <div className="compositionLabel">
+                    Create Composition {length + 1}
+                </div>
+            )
+    }
+
     return returnStatement;
 }
 
