33a9b144e9889ba3d84bdef0c6628b269e150a2d
gb23 <gregbenj@hotmail.com>
2018-02-27 11:25:52 -0700

    make CompositionLabel describe create compositions as well

--- src/components/CompositionLabel.js
+++ src/components/CompositionLabel.js
@@ -4,13 +4,31 @@ const CompositionLabel = (props) => {
     let current = props.comp.ids.indexOf(props.comp.currentId) + 1
     let length = props.comp.ids.length;
     let returnStatement = ""
-    if(props.urlId !== "new" && props.urlId !== ":id" && props.sectionId !== 0){
-        returnStatement = 
-        ( 
-            <div className="compositionLabel">
-                Composition {current} of {length}
-            </div>
-        )
+    if(props.urlId !== ":id" && props.urlId !== "new" ){
+        if(props.section.id !== 0 || (props.section.id === 0 && props.section.prev_id !== -1 && props.section.prev_id !== -2)){
+            returnStatement = 
+            ( 
+                <div className="compositionLabel">
+                    Composition {current} of {length}
+                </div>
+            )
+        }
+        else {
+            returnStatement = 
+            ( 
+                <div className="compositionLabel">
+                    Create Composition {length + 1}
+                </div>
+            )
+        }  
+    }
+    else if (props.urlId === "new"){
+            returnStatement = 
+            ( 
+                <div className="compositionLabel">
+                    Create Composition {length + 1}
+                </div>
+            )
     }
 
     return returnStatement;
